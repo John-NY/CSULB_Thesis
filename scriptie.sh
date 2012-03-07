4d56083c52cd4d50de68822a43bd6ab107f624bf
John-NY <donovan.csulb@gmail.com>
2012-03-06 17:45:15 -0800

    auto upload

--- scriptie.sh
+++ scriptie.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 DOCUMENT=thesis_draft
+PDF=Donovan_Thesis
 
 latex ${DOCUMENT}.tex
 dvips ${DOCUMENT}.dvi
 ps2pdf ${DOCUMENT}.ps
-mv ${DOCUMENT}.pdf Donovan_Thesis.pdf
+mv ${DOCUMENT}.pdf ${PDF}.pdf
 
+git commit -m 'auto upload'
+git push -u origin master
+evince ${PDF}.pdf
