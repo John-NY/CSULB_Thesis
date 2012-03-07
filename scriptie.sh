dc69b231a0e67d042168136bd2007204795fd5d0
John-NY <donovan.csulb@gmail.com>
2012-03-06 20:11:41 -0800

    auto upload

--- scriptie.sh
+++ scriptie.sh
@@ -3,11 +3,15 @@
 DOCUMENT=thesis_draft
 PDF=Donovan_Thesis
 
+bibtex ${DOCUMENT}.bib
 latex ${DOCUMENT}.tex
 dvips ${DOCUMENT}.dvi
 ps2pdf ${DOCUMENT}.ps
 mv ${DOCUMENT}.pdf ${PDF}.pdf
 
+git add ${DOCUMENT}.tex
+git add ${DOCUMENT}.bib
+git add scriptie.sh
 git commit -m 'auto upload'
 git push -u origin master
 evince ${PDF}.pdf
