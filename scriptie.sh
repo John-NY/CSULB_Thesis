5e51ae5799e1eaf1a230484840b9244b38702950
John-NY <donovan.csulb@gmail.com>
2012-09-11 13:03:13 -0700

    auto upload

--- scriptie.sh
+++ scriptie.sh
@@ -11,6 +11,9 @@ mv ${DOCUMENT}.pdf ${PDF}.pdf
 
 git add ${DOCUMENT}.tex
 git add ${DOCUMENT}.bib
+git add temperature_control.tex
+git add transport_measurements.tex
+git add appendices.tex
 git add scriptie.sh
 git add images
 git commit -m 'auto upload'
